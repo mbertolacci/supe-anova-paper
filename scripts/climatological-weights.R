91e1cfab1147395556dc0926ae4f4b8ec82f39c3
Michael Bertolacci <m.bertolacci@gmail.com>
2021-06-08 16:00:33 +1000

    Tweak weights figure to make everything fit

--- scripts/climatological-weights.R
+++ scripts/climatological-weights.R
@@ -30,11 +30,13 @@ outputs <- weight_df %>%
       ) +
       MODEL_SCALES$fill +
       theme(
-        plot.title = element_text(hjust = 0.5),
+        plot.title = element_text(hjust = 0.5, size = 10),
+        plot.margin = margin(0, 0, 0, 0, unit = 'pt'),
         axis.title.x = element_blank(),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
-        axis.text.y = element_text(size = 8),
+        axis.text.y = element_text(size = 7),
+        strip.text = element_text(size = 7),
         panel.grid.major.x = element_blank()
       ) +
       ylim(c(0, weight_max)) +
